dd387d6321ae15de76832d3a2204eb80a188b547
devkilyungi <victorkilyungi@gmail.com>
2024-05-01 10:14:48 +0300

    Initial Commit

---
+++ ConvertMeister/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConvertMeister
+//
+//  Created by Victor Kilyungi on 01/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConvertMeister/ConvertMeisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeisterApp.swift
+//  ConvertMeister
+//
+//  Created by Victor Kilyungi on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
